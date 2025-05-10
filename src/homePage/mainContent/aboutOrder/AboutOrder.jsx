8c1babc8d1902dc4da48098ccbb2e04e8cceb96c
Solomiia Kornaha <solomiya.20004@gmail.com>
2025-05-10 22:35:12 +0300

    add aboutOrder

--- src/homePage/mainContent/aboutOrder/AboutOrder.jsx
+++ src/homePage/mainContent/aboutOrder/AboutOrder.jsx
@@ -8,13 +8,18 @@ import LinkButton from '../../fourCardMenu/button/LinkButton';
 const AboutOrder = () => {
     return (
         <div className={style.mainContent}>
+
             <div className={style.backgroundImage}>
                 <img src={foodPhoto} alt="foodPhoto" className={style.foodPhoto} />
             </div>
+
             <div className={style.block}>
-                <h1 className={style.title}>ABOUT</h1>
-                <h3 className={style.subtitle}>our restaurant</h3>
-                <hr />
+
+                <div className='about'>
+                    <h1 className={style.title}>ABOUT</h1>
+                    <h3 className={style.subtitle}>our restaurant</h3>
+                    <hr />
+                </div>
 
                 <div className={style.groupIcon}>
                     <a href="tel:+12122601212" className={style.socialItem}>
@@ -33,40 +38,52 @@ const AboutOrder = () => {
 
                 <div className={style.hours}>
                     <h4 className={style.subtitle2}>HOURS</h4>
-                    <p className={style.hoursWork}>Monday — 12pm-9pm</p>
-                    <p className={style.hoursWork}>Tuesday — 12pm-9pm</p>
-                    <p className={style.hoursWork}>Wednesday — 12pm-10pm</p>
-                    <p className={style.hoursWork}>Thursday — 12pm-10pm</p>
-                    <p className={style.hoursWork}>Friday — 12pm-10pm</p>
-                    <p className={style.hoursWork}>Saturday — 12pm-10pm</p>
-                    <p className={style.hoursWork}>Sunday — 12pm-10pm</p>
+                    <div className={style.sectionHoursWork}>
+                        <p className={style.hoursWork}>Monday — 12pm-9pm</p>
+                        <p className={style.hoursWork}>Tuesday — 12pm-9pm</p>
+                        <p className={style.hoursWork}>Wednesday — 12pm-10pm</p>
+                        <p className={style.hoursWork}>Thursday — 12pm-10pm</p>
+                        <p className={style.hoursWork}>Friday — 12pm-10pm</p>
+                        <p className={style.hoursWork}>Saturday — 12pm-10pm</p>
+                        <p className={style.hoursWork}>Sunday — 12pm-10pm</p>
+                    </div>
+
                 </div>
 
-                <div className={style.phone}>
-                    <h4 className={style.subtitle2}>PHONE</h4>
-                    <p className={style.phoneNum}>212.260.1212</p>
-                    <p className={style.phoneNum}>212.260.7049</p>
+                <div className={style.sectionPhone}>
+                    <div className={style.phone}>
+                        <h4 className={style.subtitle2}>PHONE</h4>
+                        <p className={style.phoneNum}>212.260.1212</p>
+                        <p className={style.phoneNum}>212.260.7049</p>
+                    </div>
+                    <LinkButton text="ABOUT US" link="#about" />
                 </div>
-                <LinkButton text="ABOUT US" link="#about" />
             </div>
 
             <div className={style.block2}>
-                <h1 className={style.title}>ORDER</h1>
-                <h3 className={style.subtitle}>pick-up & delivery</h3>
-                <hr />
+                <div className={style.order}>
+                    <h1 className={style.title}>ORDER</h1>
+                    <h3 className={style.subtitle}>pick-up & delivery</h3>
+                    <hr />
+                </div>
 
-                <div className={style.takeOut}>
-                    <h4 className={style.subtitle2}>CONTACTLESS TAKE-OUT</h4>
-                    <p className={style.takeOutText}>To place an order for pickup,</p>
-                    <p className={style.htakeOutText}>please order via Square.</p>
+                <div className={style.sectionTakeOut}>
+                    <div className={style.takeOut}>
+                        <h4 className={style.subtitle2}>CONTACTLESS TAKE-OUT</h4>
+                        <p className={style.takeOutText}>To place an order for pickup,</p>
+                        <p className={style.takeOutText}>please order via Square.</p>
+                    </div>
                     <LinkButton text="ORDER PICKUP" link="#about" />
                 </div>
 
-                <div className={style.delivery}>
-                    <h4 className={style.subtitle2}>DELIVERY EXPANDED</h4>
-                    <p className={style.deliveryText}>To place an order for delivery, please choose from</p>
-                    <p className={style.deliveryText}>one of our partners below:</p>
-                
+                <div className={style.sectionDelivery}>
+                    <div className={style.delivery}>
+                        <h4 className={style.subtitle2}>DELIVERY EXPANDED</h4>
+                        <p className={style.deliveryText}>To place an order for delivery, please choose from</p>
+                        <p className={style.deliveryText}>one of our partners below:</p>
+                    </div>
+
+
                     <div className={style.linkDelivery}>
                         <a href="https://postmates.com/store/red-bamboo/AENNGqX0QuyQWIYlZ-oxIg?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMjIxMSUyMEF2ZW51ZSUyMEElMjIlMkMlMjJyZWZlcmVuY2UlMjIlM0ElMjIxMzFkNWMxMy04NmIzLTQ4N2QtMjZiMy03OTQ3YzhiNjEyOGElMjIlMkMlMjJyZWZlcmVuY2VUeXBlJTIyJTNBJTIydWJlcl9wbGFjZXMlMjIlMkMlMjJsYXRpdHVkZSUyMiUzQTQwLjczJTJDJTIybG9uZ2l0dWRlJTIyJTNBLTczLjk4MDY5NjclN0Q%3D&ps=1"
                             target='_blank' className={style.linkItem}>
