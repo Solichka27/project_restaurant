08135c6b87c251800437d912a586290b292c12c3
Solomiia Kornaha <solomiya.20004@gmail.com>
2025-05-12 13:41:41 +0300

    add MainContent to HomePage

--- src/homePage/homePage/HomePage.jsx
+++ src/homePage/homePage/HomePage.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import style from './HomePage.module.css';
 import Header from '../header/Header';
 import FourCardMenu from '../fourCardMenu/FourCardMenu';
-import AboutOrder from '../mainContent/aboutOrder/AboutOrder';
+import MainContent from '../mainContent/MainContent'
 
 function HomePage() {
     return (
         <div className={style.container}>
             <Header />
             <FourCardMenu />
-            <AboutOrder />
+            <MainContent />
         </div>
     );
 }
