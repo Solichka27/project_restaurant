44fc1af56630947217cc08ddd9373a3d3e80cfea
Solomiia Kornaha <solomiya.20004@gmail.com>
2025-05-18 13:56:51 +0300

    add carousel to ItemPost

--- src/homePage/mainContent/instagramPosts/itemPost/ItemPost.jsx
+++ src/homePage/mainContent/instagramPosts/itemPost/ItemPost.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Slider from "react-slick";
 import style from './ItemPost.module.css';
 import ItemPostConst from './ItemPostConst';
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
