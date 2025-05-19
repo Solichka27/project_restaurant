d79a1941fee79ae9f1ff07d08733a1b2d10d0914
Solichka27 <113928269+Solichka27@users.noreply.github.com>
2025-05-19 16:22:04 +0300

    update ItemPostConst

--- src/homePage/mainContent/instagramPosts/itemPost/ItemPostConst.jsx
+++ src/homePage/mainContent/instagramPosts/itemPost/ItemPostConst.jsx
@@ -1,27 +1,27 @@
 const ItemPostConst = [
     {
-        img: "https://scontent.cdninstagram.com/v/t51.75761-15/494591977_18499852921033929_1678936976496214659_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=109&ccb=7-5&_nc_sid=18de74&_nc_ohc=OlFA_u71EikQ7kNvwEUpka1&_nc_oc=Adnrxame6Bi2H8acxdDyD6tl6yCiEBQpGUW5gmYz87yP32dgOcu10rnFhkPXMnXb6aE&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=UCw7x4vnFNTrEcWWmg2JzA&oh=00_AfK6xLyzOuhOUfVqHoKYmYgyxRIHFAMl9AhCogyOCZsRmg&oe=682A3191",
+        img: "https://scontent-iad3-1.cdninstagram.com/v/t51.75761-15/494591977_18499852921033929_1678936976496214659_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=109&ccb=7-5&_nc_sid=18de74&_nc_ohc=_9zPPhTeePYQ7kNvwH1Eklt&_nc_oc=AdlJ0NxenrcNBhmvZxLky93sJ7XaH5JqLVx9R3OQAV9_yWLmzQ6D6aYr2hE-_-dcufg&_nc_zt=23&_nc_ht=scontent-iad3-1.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=kh7M9sXM1Au00AZiCEJjYA&oh=00_AfKlvcQ8Zoqd1H4Uf4dPfJ7aof8NNCfSGU5PryS8WPbgmQ&oe=68310151",
         url: "https://www.instagram.com/reel/DJHetoSJoDP/",
         text: "Happy AAPI Heritage Month! ❤️💙💛🤍 We are honoring our Filipino background with a special 100% vegan Filipino menu featuring",
         hashtags: "#lumpiashanghai #karekare #tocino #bbqporkskewers #tangtolang #porkadobo #chickenadobo",
         data: "May 1, 2025"
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t51.75761-15/491443368_18498227458033929_8848963627721994755_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=108&ccb=7-5&_nc_sid=18de74&_nc_ohc=4v09P11Yf_YQ7kNvwHXEZOn&_nc_oc=AdlIu2DEUsouiSO3rWjm2jox2EQwvoDg-ZNyeLJahyu3zDr03p4fIttrSZ6cQwx11T4&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=9tcjdfiOG67E3TL6aHXA3w&oh=00_AfJqS5ExTJvOIg8WEeECh6AT02EWWrXLu4rDK3B_nfI20g&oe=682A2E5F",
+        img: "https://scontent-iad3-1.cdninstagram.com/v/t51.75761-15/491443368_18498227458033929_8848963627721994755_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=108&ccb=7-5&_nc_sid=18de74&_nc_ohc=zUUR867nw6YQ7kNvwFf0ONs&_nc_oc=AdmuCJt9Fgm7reLwswJpOZFgtgkPaaXXLw6TSVaaj81U32wD_C079rfkRUx-Ff0YZbs&_nc_zt=23&_nc_ht=scontent-iad3-1.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=kh7M9sXM1Au00AZiCEJjYA&oh=00_AfJ8goBYt_7YYgz5Ip1KWJGD3bsBksDi5fRBmRcpNrixSQ&oe=6830FE1F",
         url: "https://www.instagram.com/p/DIwMq_AMGEO/",
         text: "Happy Earth Day from the boo! 🌎🥰 Today we are celebrating all the ways that eating vegan food helps the planet. Use the code “REDBAMBOOLOVESYOU” for 10% off your takeout order to enjoy something delicious and planet healthy 😋",
         hashtags: "#earthday #veganfortheplanet #climatechange #veganfood #vegandiet #lovetheearth #vegan #vegancommunity #veganlove #nyc #earthdaynyc #earthday2025 #veganmemeshare #infographics #discountcode",
         data: "April 22, 2025"
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t51.75761-15/491443254_18497209354033929_4185959081424495683_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=102&ccb=7-5&_nc_sid=18de74&_nc_ohc=gHA23Nx4Oy0Q7kNvwE02aqE&_nc_oc=AdkbivPsRa0R-r5TfNTQ45Ft1HLpxdNUHc38LULovpTwd4N5F4ORZA-7iEzcddctOhI&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=9tcjdfiOG67E3TL6aHXA3w&oh=00_AfKnmOwxwIMGvnOU6chp4Da4bHDrxuclQuLNZFhsbYn4BQ&oe=682A5F39",
+        img: "https://scontent-iad3-1.cdninstagram.com/v/t51.75761-15/491443254_18497209354033929_4185959081424495683_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=102&ccb=7-5&_nc_sid=18de74&_nc_ohc=rR8KhsKNtXcQ7kNvwGsYKS1&_nc_oc=AdmhVOef_KnZb-f3WjTBWqVxx04Zs49HUU-sh6YmQvKhHtArUvM6qzPegIRzNpX_FQE&_nc_zt=23&_nc_ht=scontent-iad3-1.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=kh7M9sXM1Au00AZiCEJjYA&oh=00_AfLj5oUJJAAY_vPCeMRTRS6e_ONjSaU1Odrkd8Buhcu_fA&oe=6830F6B9",
         url: "https://www.instagram.com/reel/DIguUMgOw5G/",
         text: "WOW! With crispy french fries, melted cheddar cheese, chopped bacon and scallions our NEW loaded fries are a must try 🍟🧀😋",
         hashtags: " ",
         data: "April 16, 2025"
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t51.75761-15/490757375_18496700233033929_8030742526465816896_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=109&ccb=7-5&_nc_sid=18de74&_nc_ohc=_o9sWEv5L74Q7kNvwHuiZDN&_nc_oc=AdlrC8T0y-u8HJ7oI_7QYI6VqgNJaQknDoCiipQvHncCk46K_A_Tm3Ax28o91o_qyc0&_nc_zt=23&_nc_ht=scontent.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=9tcjdfiOG67E3TL6aHXA3w&oh=00_AfLWrpn7_T6MuOviKZhWHYCzZ6tmy2zvxvNBySZ80PtTSQ&oe=682A5599",
+        img: "https://scontent-iad3-1.cdninstagram.com/v/t51.75761-15/490757375_18496700233033929_8030742526465816896_n.jpg?stp=dst-jpg_e35_tt6&_nc_cat=109&ccb=7-5&_nc_sid=18de74&_nc_ohc=N42TkgeLNIIQ7kNvwGQfIbT&_nc_oc=AdlsPsB55sieWOy4uZxCkm7U53Dgq_vuAchsfaFLdCJtVgOPs3rY80e5DZqafPZTtIc&_nc_zt=23&_nc_ht=scontent-iad3-1.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=kh7M9sXM1Au00AZiCEJjYA&oh=00_AfKZ46g8wc-qf6lFdWTcn2uClx1pOr6HgO7BY5UDwbearg&oe=6830ED19",
         url: "https://www.instagram.com/p/DIbfgGssbYq/",
         text: "Modeled here by our lovely staff you can now take your merch hot to go to help support the boo! ❤️👕 Order your new favorite shirt, hoodie, hat and/or mug in a variety of colors at the link bio 🔗",
         hashtags: "#lumpiashanghai #karekare #tocino #bbqporkskewers #tangtolang #porkadobo #chickenadobo",
