b5c82699f6620d5da7f1cdfacfc428d78c23f867
Solomiia Kornaha <solomiya.20004@gmail.com>
2025-11-04 20:39:17 +0200

    add animation home page

--- src/pages/homePage/mainPhoto/MainPhoto.jsx
+++ src/pages/homePage/mainPhoto/MainPhoto.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import mainPhoto from '../../../assets/image/homePage/header/mainPhoto.png';
 import logo from '../../../assets/image/homePage/header/logo.png';
 import style from './MainPhoto.module.css';
 
 const MainPhoto = (props) => {
     return (
         <div className={style.mainContainer}>
-            <img src={mainPhoto} alt="mainPhoto" className={style.mainPhoto} />
+            <div className={style.mainPhoto}> </div>
             <img src={logo} alt="mainPhoto" className={style.logo} />
         </div >
     )
